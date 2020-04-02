42046eb4ee5d80bc18c124dc74b787282605694b
alex hayes <alexpghayes@gmail.com>
2020-04-02 13:27:03 -0500

    Add bff function following @fchen365's code

---
+++ R/bff.R
@@ -0,0 +1,49 @@
+#' Find features most associated with cluster membership
+#'
+#' @param loadings An `n` by `k` matrix of weights that indicates how
+#'   important that ith user is to the jth cluster, i.e., the `Z` or `Y`
+#'   matrix calculated by [vsp()].
+#'
+#' @param features An `n` by `d` matrix of features measured for each
+#'   node in the network.
+#
+#' @param num_best An integer indicating how many of the top features
+#'   for differentiating between loadings you want.
+#'
+#' @return An `n` by `k` matrix whose `[i, j]` entry is the
+#'   ith "most important" feature for cluster j.
+#'
+#' @export
+bff <- function(loadings, features, num_best, ...) {
+  UseMethod("bff")
+}
+
+l1_normalize <- function(x) x / sum(x)
+
+#' @export
+bff.default <-  function(loadings, features, num_best){
+
+  # Fan has this line in his code but I don't understand why?
+  loadings[loadings < 0] <-  0
+  k <- ncol(loadings)
+
+  best_feat <- matrix("", ncol = k, nrow = num_best)
+
+  ## normalize the cluster to a probability distribution (select one member at random)
+  nc <- apply(loadings, 2, l1_normalize)
+  nOutC <- apply(loadings == 0, 2, l1_normalize)
+
+  inCluster <- sqrt(crossprod(features, nc))
+  outCluster <- sqrt(crossprod(features, nOutC))
+
+  # diff is d x K, element [i,j] indicates importance of i-th feature to j-th block
+  # contrast: sqrt(A) - sqrt(B), variance stabilization
+  diff <-  inCluster - outCluster
+
+  for (j in 1:k) {
+    bestIndex <-  order(-diff[,j])[1:num_best]
+    best_feat[,j] <-  colnames(features[, bestIndex])
+  }
+
+  best_feat
+}
--- scratch/bff-scratch.R
+++ scratch/bff-scratch.R
@@ -19,14 +19,47 @@ four_core <- graph %>%
     coreness = node_coreness(mode = "all")
   ) %>%
   filter(coreness >= 4) %>%
-  left_join(papers, by = c("name" = "DOI"))
+  left_join(papers, by = c("name" = "DOI")) %>%
+  filter(!is.na(abstract))
 
 four_core_fa <- vsp(four_core, k = 25)
 
 screeplot(four_core_fa) +
   ylim(0.4, 0.7)
 
-# TODO: add the BFF function
+# get the features matrix
+
+library(tm)
+library(tidytext)
+
+# don't mind the eigengap -- remove words that appear in fewer than 10
+# documents
+
+dtm <- four_core %>%
+  activate(nodes) %>%
+  as_tibble() %>%
+  unnest_tokens(token, abstract, token = "tweets") %>%
+  group_by(name) %>%
+  count(token) %>%
+  cast_sparse(name, token, n)
+
+frequent_words <- colSums(sign(dtm)) > 10
+
+length(frequent_words)
+sum(frequent_words)
+
+dtm_freq <- dtm[, which(frequent_words)]
+
+bff(four_core_fa$Z, dtm_freq, 5)
+
+dim(four_core_fa$Z)
+dim(dtm_freq)
+
+papers %>%
+  filter(!is.na)
+  count(is.na(abstract))
+
+tm
 
 # consider the 4 core
 coreness(graph)
